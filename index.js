f7ec4276787746b88b55266724bc587f239a94d2
Steven Finley <steve@Stevens-MacBook-Pro.local>
2019-04-02 11:09:17 -0500

    www redirect in index.js

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")))
-app.set("views", path.join(__dirname, "dist"))
-app.set("view engine", "html")
-app.get("/", (req, res) => res.render("dist/index"))
+app.all(/.*/, wwwredirect);
+app.use(express.static(path.join(__dirname, "dist")));
+app.set("views", path.join(__dirname, "dist"));
+app.set("view engine", "html");
+app.get("/", (req, res) => res.render("dist/index"));
 app.get("/resume", (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'Steven_Finley_CV.pdf'));
+  res.sendFile(path.join(__dirname, "Steven_Finley_CV.pdf"));
 });
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function wwwredirect(req, res, next) {
+  var host = req.header("host");
+  if (host.match(/^www\..*/i)) {
+    next();
+  } else {
+    res.redirect(301, "http://www." + host + req.url);
+  }
+}
