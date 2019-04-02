671a173268d158bdbc8b2bff76d6b0e5ac0f286d
Steven Finley <steve@Stevens-MacBook-Pro.local>
2019-04-02 08:54:30 -0500

    readme update. profile update..

---
+++ README..md
@@ -0,0 +1,12 @@
+My Portfolio Website
+====
+
+### Tech
+
+* [Node-Sass] - SCSS compiler
+* [Express] - fast node.js network app framework
+* [Node.js] - Node server for the back-end
+
+[node-sass]: <https://github.com/sass/node-sass>
+[express]: <http://expressjs.com>
+[node.js]: <http://nodejs.org>
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-express()
-  .use(express.static(path.join(__dirname, "dist")))
-  .set("views", path.join(__dirname, "dist"))
-  .set("view engine", "html")
-  .get("/", (req, res) => res.render("dist/index"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")))
+app.set("views", path.join(__dirname, "dist"))
+app.set("view engine", "html")
+app.get("/", (req, res) => res.render("dist/index"))
+app.get("/resume", (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'Steven_Finley_CV.pdf'));
+});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
